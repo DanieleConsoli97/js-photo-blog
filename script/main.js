4530f11097e85a7ab43fe2670f2a09fe825c5e0f
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-30 15:14:16 +0100

    add css ,html and js

--- script/main.js
+++ script/main.js
@@ -1,31 +1,35 @@
-const cards = document.getElementById("cards_main")
-   
-console.log(cards)
-axios
-      .get("https://lanciweb.github.io/demo/api/pictures/")
-      .then((rensponse) => {
-       
-         const data = rensponse.data
-         data.forEach(element => {
-           
-            cards.innerHTML+=
-            `
-            <div class="card">
+const cards = document.querySelector(".cards_main");
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((rensponse) => {
+  const data = rensponse.data;
+  let cardsIt = [];
+  let modalAppend = document.querySelector("modale")
+  data.forEach((element) => {
+    const { date, id, title, url } = element;
+    cards.innerHTML += `
+            <div class="card" data-attribute="${id}">
                     <img class="circle" src="./img/background/pin.svg" alt="">
                     <picture class="container_img">
-                        <img class="img" src=${element.url} alt="">
+                        <img class="img" src=${url} alt="">
                     </picture>
                     <div class="text">
                         <div class="date_cont">
-                            <p class="date">${element.date}</p>
+                            <p class="date">${date}</p>
                         </div>
                         <div class="place_cont">
-                            <p class="place">${element.title}</p>
+                            <p class="place">${title}</p>
                         </div>
                     </div>
                 </div>
-            `
+            `;
+    cardsIt = document.querySelectorAll(".img");
+  });
+  cardsIt.forEach((element) => {
+    element.addEventListener("click", function () {
+    
+        console.log("prova",this.src);
     });
+  });
+});
 
-      });
-     
+    
